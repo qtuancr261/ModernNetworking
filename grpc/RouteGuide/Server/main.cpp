ec4df80f043aac009ae6b60c1f69e5ce0d4ede91
qtuancr261 <tuancr261@gmail.com>
2024-04-25 16:07:48 +0700

    update: using grpc::ResourceQuota to set max thread for sync server

--- grpc/RouteGuide/Server/main.cpp
+++ grpc/RouteGuide/Server/main.cpp
@@ -1,15 +1,20 @@
 //
 // Created by tuantq on 15/04/2024.
 //
-#include "RouteGuideServer.h"
 #include "Helper.h"
+#include "RouteGuideServer.h"
 #include <grpcpp/server_builder.h>
 #include <iostream>
+#include <thread>
 
 void RunSyncRouteGuideServer(const std::string &db_path) {
     std::string server_address{"0.0.0.0:1943"};
     RouteGuideServer syncService{db_path};
     grpc::ServerBuilder serverBuilder;
+    //
+    grpc::ResourceQuota resourceQuota;
+    resourceQuota.SetMaxThreads(std::thread::hardware_concurrency());
+    serverBuilder.SetResourceQuota(resourceQuota);
     serverBuilder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     serverBuilder.RegisterService(&syncService);
     std::unique_ptr<grpc::Server> server(serverBuilder.BuildAndStart());
